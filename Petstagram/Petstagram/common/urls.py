adb0ed5be9420d99dbf489ee2d7861242800ed7e
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-06-22 17:24:05 +0300

    commiting
    commiting

--- Petstagram/Petstagram/common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from Petstagram.common import views
-
-
-urlpatterns = [
-    path('', views.landing_page, name='landing'),
-]
--- Petstagram/Petstagram/pets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Petstagram.pets.models import Pet, Like
-
-# Register your models here.
-admin.site.register(Pet)
-admin.site.register(Like)
